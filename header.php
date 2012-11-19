0baee899dfeca2878e711180dd624f999430f9b4
Stephen Finney <stephen@codeforamerica.org>
2012-11-19 18:27:13 -0500

    Removed comment

--- header.php
+++ header.php
@@ -73,7 +73,7 @@
     <div id="header">
       <p class="top-menu"><a href="<?php echo home_url(); ?>">HOME</a> / <a href="/splost/about-splost/">ABOUT SPLOST</a> / <a href="/splost/about/">ABOUT SITE</a> / <a href="/splost/contact/">CONTACT</a> / <a href="/tag/press/">PRESS</a> / <a href="/splost/updates/">UPDATES</a> </p> 
       <div id="title_container">
-        <!-- <p class="subtitle">Macon-Bibb County</p> --> <div style="clear:both;"></div>
+      <div style="clear:both;"></div>
       <h1 class="tright">Macon-Bibb County 2012 SPLOST</h1>
       
       </div>
